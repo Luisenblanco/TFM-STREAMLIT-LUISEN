dfe7a13bdb1d7bd1b48d279f5713952fa7b125a9
Luisenblanco <163674138+Luisenblanco@users.noreply.github.com>
2024-03-23 10:42:03 +0100

    Update GRAFICO DE BARRAS-COMPARATIVA.py

--- pages/GRAFICO DE BARRAS-COMPARATIVA.py
+++ pages/GRAFICO DE BARRAS-COMPARATIVA.py
@@ -69,25 +69,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-jugadores = ['Cote', 'Pablo Pérez', 'Rafael Obrador', 'D. van der Kust', 'S. Laquidaín']
-datos = [
-    [4.06, 0.18, 14.79, 2.92, 2.6, 0.27, 0.14, 4.89, 0.59, 6.67],
-    [3.23, 0.06, 18.42, 3.59, 2.39, 0.36, 0.06, 5.02, 0.36, 7.71],
-    [4.56, 0.24, 15.78, 3.02, 3.74, 0.86, 0.19, 4.89, 0.24, 7.24],
-    [3.11, 0.1, 20.26, 4.24, 2.86, 0.44, 0.35, 6.11, 0.59, 7.98],
-    [0.27, 0.0, 18.68, 5.18, 0.75, 0.0, 0.0, 5.28, 0.21, 8.17]
-]
-
-# Convertir los datos a un DataFrame de pandas
-df = pd.DataFrame(datos, columns=['Centros', 'Asistencias', 'Duelos', 'Pases en último tercio',
-                                   'Acciones de ataque', 'Jugadas claves', 'Desmarques', 'Interceptaciones',
-                                   'Pases en profundidad', 'Pases progresivos'], index=jugadores)
-
-# Definir los nombres de los jugadores y las métricas
-nombres_deseados = ['Cote', 'Pablo Pérez', 'Rafael Obrador', 'D. van der Kust', 'S. Laquidaín']
-metricas_seleccionadas = ['Centros', 'Asistencias', 'Duelos', 'Pases en último tercio',
-                          'Acciones de ataque', 'Jugadas claves', 'Desmarques', 'Interceptaciones',
-                          'Pases en profundidad', 'Pases progresivos']
 
 # Sidebar.expander para seleccionar jugadores
 with st.sidebar.expander("🥾⚽ Seleccionar Jugadores"):
